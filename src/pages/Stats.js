fcd0130505a622707769f05687addd5133eec67f
bigpoppa-sys <chris@syscoin.org>
2021-05-03 21:51:46 +1000

    Update Stats.js

--- src/pages/Stats.js
+++ src/pages/Stats.js
@@ -65,12 +65,8 @@ export class Stats extends Component {
                         <meta name="description" content="Sysnode.info provides Syscoin Masternode Operators the tools to maximise the most from their Masternodes!" />
                     </MetaTags>
                     <InnerBanner heading="Stats"/>
-                    /*<Doughnut chartData={this.state.api_data.stats.mn_stats}/>*/
                     <Income incomeData={this.state.api_data.stats.income_stats} incomeSenOneYrData={this.state.api_data.stats.income_stats_seniority_one_year}/>
-                    
                     <Price priceData={this.state.api_data.stats.price_stats}/>
-                    /*<AxisChart datesData={this.state.api_dates_data[0]} datal={this.state.dataload} title={'Nodes Created Each Day'}/>
-                     <AxisChart datesData={this.state.api_dates_data[1]} datal={this.state.dataload} title={'Total nodes each day'}/> */
                     <Investment investData={this.state.api_data.stats.mn_stats} blockchainData={this.state.api_data.stats.blockchain_stats}/>
                     <WorldMap mapData={this.state.api_data.mapData} mapFills={this.state.api_data.mapFills}/>
                     
